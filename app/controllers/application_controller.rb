927dda1b79712d3e676a8adcd1817e11a81190e0
Trevor Vardeman <trevortx@gmail.com>
2022-07-24 19:33:59 -0700

    created controller for handling patch requests for appointments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,21 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   get "/" do
-    # { message: "Good luck with your project!" }.to_json
+  end
+
+  get "/appointments" do
+    Appointment.upcoming_appointments.to_json(include: [:service, :dog, :groomer])
+  end
+
+  get "/appointments/:id" do
+    appointment = Appointment.find(params[:id])
+    appointment.to_json(include: [:service, :dog, :groomer])
+  end
+
+  delete "/appointments/:id" do
+    appointment = Appointment.find(params[:id])
+    appointment.destroy
+    appointment.to_json
   end
 
   get "/create-appointment" do
@@ -16,6 +30,28 @@ class ApplicationController < Sinatra::Base
     data.to_json
   end
 
+  get "/appointments/edit/:id" do
+    data = {}
+
+    dogs = Dog.all
+    groomers = Groomer.all
+    services = Service.all
+
+    data = dogs, groomers, services
+    data.to_json
+  end
+
+  patch "/appointments/edit/:id" do
+    appointment = Appointment.find(params[:id])
+    appointment.update(
+      dog_id: params[:dog_id],
+      groomer_id: params[:groomer_id],
+      appt_datetime: params[:appt_datetime],
+      service_id: params[:service_id]
+    )
+    appointment.to_json
+  end
+
   post "/create-appointment" do
     appointment = Appointment.create(
       dog_id: params[:dog_id],
@@ -23,29 +59,6 @@ class ApplicationController < Sinatra::Base
       appt_datetime: params[:appt_datetime],
       service_id: params[:service_id]
     )
-  end
-
-  get "/appointments" do
-    Appointment.upcoming_appointments.to_json(include: [:service, :dog, :groomer])
-  end
-
-  get "/appointments/:id" do
-    appointment = Appointment.find(params[:id])
-    appointment.to_json(include: [:service, :dog, :groomer])
-  end
-
-  patch "/appointments/:id" do
-    appointment = Appointment.find(params[:id])
-    appointment.update(
-
-    )
-    appointment.to_json
-  end
-
-  delete "/appointments/:id" do
-    appointment = Appointment.find(params[:id])
-    appointment.destroy
-    appointment.to_json
   end
 
   get "/past-appointments" do
